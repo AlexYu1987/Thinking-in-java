bc37a2508e13915aa9ad0775e2b159c49717b0a3
AlexYu87 <alexyu87@outlook.com>
2017-02-05 22:23:27 +0800

    Implement LinkedStack

--- src/main/java/org/yzj/thinking/generics/LinkedStack.java
+++ src/main/java/org/yzj/thinking/generics/LinkedStack.java
@@ -1,7 +1,49 @@
 package org.yzj.thinking.generics;
 
+import java.util.Arrays;
+
 /**
  * Created by yuzhoujun on 2017/2/5.
  */
-public class LinkedStack {
+public class LinkedStack<T> {
+    private  class Node {
+        T item;
+        Node next;
+
+        Node() {
+            item = null;
+            next = null;
+        }
+
+        Node(T item, Node next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    private Node top = new Node();
+
+    public void push(T item) {
+        top = new Node(item, top);
+    }
+
+    public T pop() {
+        T result = top.item;
+        top = top.next;
+
+        return result;
+    }
+
+    public static void main(String... arg){
+        LinkedStack<String> stack = new LinkedStack<String>();
+
+        for (String s : "I am a programmer.".split(" ")) {
+            stack.push(s);
+        }
+
+        String str;
+        while ((str = stack.pop()) != null) {
+            System.out.print(str);
+        }
+    }
 }
